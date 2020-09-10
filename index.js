184940fe0dc6af942d99108c5a0942bd6bfcfc40
Ma7eer <maher.alkendi2@gmail.com>
2020-09-11 03:45:47 +0400

    e

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public");
+    cb(null, path.join(__dirname, "/public"));
   },
   filename: function (req, file, cb) {
     NEW_FILENAME = Date.now() + "-" + file.originalname;
